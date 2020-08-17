90aa086b80abedff7664008d79273a058f009b86
Fred Goldstein-Rose <goldsteinrose@cs.uchicago.edu>
2020-08-16 23:19:56 -0400

    s3 instead of directly returing image

--- api/rest-api.js
+++ api/rest-api.js
@@ -5,40 +5,49 @@ const {v4 : uuid} = require('uuid');
 const frac = require('./frac');
 const {Worker, isMainThread, workerData} = require('worker_threads');
 const Jimp = require('jimp');
+const aws = require('aws-sdk');
+const S3_BUCKET = process.env.S3_BUCKET_NAME;
 
+aws.config.region='us-east-2';
 
 router.post('/api', (req, res) => {
     const name = uuid();
     let options = req.body;
     options.name = name;
 
-    /*
-    fs.writeFile(`${__dirname}/../www/images/${name}.json`, JSON.stringify(req.body), (err) => {
-        if (err) throw err;
-        //console.log('completed json file write');
-    });
-    */
-
     let fractal = frac.runFractal(options);
+
     if(fractal == null){
         res.status(400);
         res.send("Request failed");
     }
     else{
-        fractal.getBase64(Jimp.MIME_PNG, (err, img) => {
-            res.status(200);
-            res.json({'img' : img});});
-    }
-    /*
-    const worker = new Worker(`${__dirname}/frac.js`,
-    {
-        workerData: options
-    })
-    */
+        fractal.getBuffer(Jimp.MIME_PNG, (err, img) => {
+            const s3 = new aws.S3();
+            const s3Paramsjson = {
+                Bucket: S3_BUCKET,
+                Key: name + ".json",
+                Body: JSON.stringify(options),
+                ContentType: "application/json"
+            }
 
-    //res.json({'url' : `images/${name}.png`});
-    
-    
+            const s3Paramsimg = {
+                Bucket: S3_BUCKET,
+                Key: name + ".png",
+                Body: img,
+                ContentType: "image/png",
+                ACL:'public-read'
+            }
+            s3.upload(s3Paramsjson, (err, data) =>{
+                if (err) throw err;
+            });
+            s3.upload(s3Paramsimg, (err, data) =>{
+                if (err) throw err;
+            });
+
+            res.status(200);
+            res.json({'url' : `https://fraquilt.s3.us-east-2.amazonaws.com/${name}.png`});});
+    }
 })
 
 module.exports = router;
