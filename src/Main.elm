05953cea15cf7ad68165a289efaa931ac24c17b7
Fred Goldstein-Rose <fgoldrose@gmail.com>
2024-01-24 20:18:18 -0500

    Added sliders for adjusting initial vars and level

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,6 @@
 module Main exposing (..)
 
+import Array
 import Browser
 import ColorAdjustments exposing (ColorAdjustments)
 import Html exposing (Html)
@@ -18,10 +19,12 @@ type alias Model =
 type Msg
     = NoOp
     | Randomize
+    | UpdateInitialVar Int String
+    | ChangeLevel String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
+update msg ({ settings, randomSeed } as model) =
     case msg of
         NoOp ->
             ( model, Cmd.none )
@@ -31,15 +34,48 @@ update msg model =
                 ( randomSettings, newSeed ) =
                     Random.step
                         (Settings.random
-                            { numVars = 4, level = model.settings.level }
+                            { numVars = 4, level = settings.level }
                         )
-                        model.randomSeed
+                        randomSeed
             in
             ( { settings = randomSettings
               , randomSeed = newSeed
               }
             , Settings.render randomSettings
             )
+
+        UpdateInitialVar index stringValue ->
+            case String.toInt stringValue of
+                Nothing ->
+                    ( model, Cmd.none )
+
+                Just value ->
+                    let
+                        newSettings =
+                            { settings
+                                | initialVariables =
+                                    Array.set index value settings.initialVariables
+                            }
+                    in
+                    ( { model | settings = newSettings }
+                    , Settings.render newSettings
+                    )
+
+        ChangeLevel levelString ->
+            case String.toInt levelString of
+                Nothing ->
+                    ( model, Cmd.none )
+
+                Just value ->
+                    let
+                        newSettings =
+                            { settings
+                                | level = value
+                            }
+                    in
+                    ( { model | settings = newSettings }
+                    , Settings.render newSettings
+                    )
 
 
 view : Model -> Html Msg
@@ -52,7 +88,12 @@ view model =
         , HA.style "height" "100vh"
         , HA.style "gap" "16px"
         ]
-        [ Settings.viewEditSettings model.settings
+        [ Settings.viewEditSettings
+            { settings =
+                model.settings
+            , updateInitialVar = UpdateInitialVar
+            , changeLevel = ChangeLevel
+            }
         , Html.div
             [ HA.style "width" "512px"
             , HA.style "height" "512px"
--- src/Settings.elm
+++ src/Settings.elm
@@ -1,8 +1,10 @@
 port module Settings exposing (..)
 
+import Array exposing (Array)
 import ColorAdjustments exposing (ColorAdjustments)
 import Html exposing (Html)
 import Html.Attributes as HA
+import Html.Events as HE
 import Json.Encode as Encode
 import Random
 
@@ -12,7 +14,7 @@ port renderImage : Encode.Value -> Cmd msg
 
 type alias Settings =
     { level : Int
-    , initialVariables : List Int
+    , initialVariables : Array Int
     , tl : ColorAdjustments
     , tr : ColorAdjustments
     , bl : ColorAdjustments
@@ -25,7 +27,7 @@ render settings =
     renderImage <|
         Encode.object
             [ ( "level", Encode.int settings.level )
-            , ( "initialVariables", Encode.list Encode.int settings.initialVariables )
+            , ( "initialVariables", Encode.array Encode.int settings.initialVariables )
             , ( "colorAdjustments"
               , Encode.object
                     [ ( "tl", ColorAdjustments.encode settings.tl )
@@ -37,18 +39,113 @@ render settings =
             ]
 
 
-viewEditSettings : Settings -> Html msg
-viewEditSettings settings =
+viewEditSettings :
+    { settings : Settings
+    , updateInitialVar : Int -> String -> msg
+    , changeLevel : String -> msg
+    }
+    -> Html msg
+viewEditSettings { settings, updateInitialVar, changeLevel } =
+    let
+        colorAdjustmentsGrid =
+            Html.div
+                [ HA.style "display" "grid"
+                , HA.style "grid-template-columns" "1fr 1fr"
+                , HA.style "grid-gap" "50px"
+                ]
+                [ ColorAdjustments.view settings.tl
+                , ColorAdjustments.view settings.tr
+                , ColorAdjustments.view settings.bl
+                , ColorAdjustments.view settings.br
+                ]
+
+        initialVars =
+            Html.div
+                [ HA.style "display" "flex"
+                , HA.style "flex-direction" "column"
+                , HA.style "align-items" "center"
+                , HA.style "gap" "10px"
+                ]
+                (List.indexedMap
+                    (\index initVar ->
+                        let
+                            id =
+                                "initVar-" ++ String.fromInt index
+                        in
+                        Html.div
+                            [ HA.style "display" "flex"
+                            , HA.style "flex-direction" "row"
+                            , HA.style "align-items" "center"
+                            , HA.style "gap" "5px"
+                            ]
+                            [ Html.label
+                                [ HA.for id
+                                , HA.style "width" "30px"
+                                , HA.style "text-align" "right"
+                                ]
+                                [ Html.text (String.fromInt initVar) ]
+                            , Html.input
+                                [ HA.id id
+                                , HA.type_ "range"
+                                , HA.min "0"
+                                , HA.max "255"
+                                , HA.value (String.fromInt initVar)
+                                , HE.onInput (updateInitialVar index)
+                                ]
+                                []
+                            ]
+                    )
+                    (Array.toList settings.initialVariables)
+                )
+
+        level =
+            Html.div
+                [ HA.style "display" "flex"
+                , HA.style "flex-direction" "column"
+                , HA.style "align-items" "center"
+                , HA.style "gap" "10px"
+                ]
+                [ Html.span [] [ Html.text "Level" ]
+                , Html.div
+                    [ HA.style "display" "flex"
+                    , HA.style "flex-direction" "row"
+                    , HA.style "align-items" "center"
+                    , HA.style "gap" "5px"
+                    ]
+                    [ Html.label
+                        [ HA.for "level"
+                        , HA.style "width" "30px"
+                        , HA.style "text-align" "right"
+                        ]
+                        [ Html.text (String.fromInt settings.level) ]
+                    , Html.input
+                        [ HA.id "level"
+                        , HA.type_ "range"
+                        , HA.min "1"
+                        , HA.max "10"
+                        , HA.list "level-markers"
+                        , HA.value (String.fromInt settings.level)
+                        , HE.onInput changeLevel
+                        ]
+                        []
+                    , Html.datalist [ HA.id "level-markers" ]
+                        (List.map
+                            (\l ->
+                                Html.option
+                                    [ HA.value (String.fromInt l) ]
+                                    [ Html.text (String.fromInt l) ]
+                            )
+                            (List.range 1 10)
+                        )
+                    ]
+                ]
+    in
     Html.div
-        [ HA.style "display" "grid"
-        , HA.style "grid-template-columns" "1fr 1fr"
-        , HA.style "grid-gap" "50px"
+        [ HA.style "display" "flex"
+        , HA.style "flex-direction" "column"
+        , HA.style "gap" "50px"
         ]
-        [ ColorAdjustments.view settings.tl
-        , ColorAdjustments.view settings.tr
-        , ColorAdjustments.view settings.bl
-        , ColorAdjustments.view settings.br
-        ]
+        [ colorAdjustmentsGrid, initialVars, level ]
 
 
 random : { numVars : Int, level : Int } -> Random.Generator Settings
@@ -63,7 +160,7 @@ random { numVars, level } =
             , br = br
             }
         )
-        (Random.list numVars (Random.int 0 255))
+        (Random.map Array.fromList (Random.list numVars (Random.int 0 255)))
         (ColorAdjustments.random numVars)
         (ColorAdjustments.random numVars)
         (ColorAdjustments.random numVars)
